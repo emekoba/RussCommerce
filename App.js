74efa0e9189f58f4fc8ce3317bfaff04ceb41e87
emekoba <klojett.re@gmail.com>
2021-09-26 15:01:14 +0100

    implemented add to cart and remove from cart functionality.

--- App.js
+++ App.js
@@ -1,24 +1,67 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Login from './src/pages/onboarding/Login';
 import Pages from './src/pages/Pages';
 import SplashScreen from 'react-native-splash-screen';
-// console.disableYellowBox = true;
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reducer from './src/store/reducer';
+import createSagaMiddleware from '@redux-saga/core';
+import {combineSagas} from './src/sagas/rootSaga';
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
+import ProductList from './src/pages/main/Product/Product.list';
+import ProductPage from './src/pages/main/Product/Product.page';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(combineSagas);
 
 const MainNavigator = createStackNavigator({
-  Login: {screen: Login},
-  Pages: {screen: Pages},
+  Pages: {screen: Pages, navigationOptions: {headerShown: false}},
+  Login: {screen: Login, navigationOptions: {headerShown: false}},
 });
 
 const AppContainer = createAppContainer(MainNavigator);
 
 export default function App() {
-  React.useEffect(() => SplashScreen.hide());
+  useEffect(() => SplashScreen.hide());
+
+  let theme = useColorScheme();
 
   return (
-    /*<Provider store={store}>*/
-    <AppContainer />
-    /*</Provider>*/
+    <Provider store={store}>
+      <AppearanceProvider>
+        <AppContainer theme={theme} />
+        {/* <ProductPage /> */}
+      </AppearanceProvider>
+    </Provider>
   );
 }
+
+// function Epsilon({loader}) {
+//   return (
+// <Provider store={store}>
+//   <AppearanceProvider>
+//     <View style={_x.main}>
+//       <AppContainer theme={theme} />
+//     </View>
+//   </AppearanceProvider>
+// </Provider>
+//   );
+// }
+
+// const _x = StyleSheet.create({
+//   main: {
+//     flex: 1,
+//     position: 'relative',
+//   },
+// });
+
+// function mapStateToProps(state) {
+//   return {
+//     productList: state.productList,
+//   };
+// }
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,19 +1,105 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Home from './main/Product/Product.list';
+import Cart from './main/Cart/Cart';
+import {cartActive, home} from '../resources/resources';
+import {connect} from 'react-redux';
+import {globalTheme, SagaCommands} from '../globals/globals';
 
-export default function Pages() {
-  return (
-    <View style={styles.container}>
-      <Text>Pages</Text>
-    </View>
+function Pages({unseenInCart, getAllProducts}) {
+  const _theme = globalTheme['light'];
+
+  useEffect(() => getAllProducts(), []);
+
+  const TabNavigator = createBottomTabNavigator(
+    {
+      Products: {
+        screen: Home,
+        navigationOptions: () => ({
+          tabBarIcon: () => (
+            <Image source={home} style={{width: 30}} resizeMode="contain" />
+          ),
+        }),
+      },
+      Cart: {
+        screen: Cart,
+        navigationOptions: () => ({
+          tabBarIcon: () => (
+            <View style={{position: 'relative'}}>
+              <Image
+                source={cartActive}
+                style={{width: 30}}
+                resizeMode="contain"
+              />
+
+              {unseenInCart > 0 && (
+                <View style={_x(_theme).badge}>
+                  <Text style={_x(_theme).badge_txt}>{unseenInCart}</Text>
+                </View>
+              )}
+            </View>
+          ),
+        }),
+      },
+    },
+    {
+      // initialRouteName: 'Cart',
+      swipeEnabled: true,
+      animationEnabled: true,
+      tabBarOptions: {
+        showLabel: false,
+        style: {
+          borderTopWidth: 0,
+          elevation: 0,
+          height: 60,
+        },
+      },
+    },
   );
+
+  const AppTab = createAppContainer(TabNavigator);
+
+  return <AppTab />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function mapStateToProps(state) {
+  return {
+    unseenInCart: state.cart.unseen,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getAllProducts: () =>
+      dispatch({
+        type: SagaCommands.FETCH_PRODUCTS,
+        limit: 10,
+      }),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Pages);
+
+const _x = theme => {
+  return StyleSheet.create({
+    badge: {
+      backgroundColor: theme?.primary,
+      height: 20,
+      width: 20,
+      borderRadius: 20,
+      position: 'absolute',
+      top: 10,
+      left: 20,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    badge_txt: {
+      color: theme?.badge_text,
+      fontWeight: 'bold',
+      fontSize: 10,
+    },
+  });
+};
